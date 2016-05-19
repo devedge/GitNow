968ca00648a5025e8286a28e0188976904f70e29
devedge <devedge@outlook.com>
2016-05-18 23:43:10 -0400

    Fixed header, but it's too big

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ app.on('ready', function() {
   mainWindow = new BrowserWindow({width: 800, height: 600, resizable: false});
 
   // and load the index.html of the app.
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  mainWindow.loadURL('file://' + __dirname + '/views/main.html');  //index.html
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools();
