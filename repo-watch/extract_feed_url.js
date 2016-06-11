cdbedcfc364037e48cb4304ba4cc1f1ba02998a5
devedge <devedge@outlook.com>
2016-06-10 22:28:24 -0400

    notes for FeedFormat added

--- repo-watch/extract_feed_url.js
+++ repo-watch/extract_feed_url.js
@@ -4,5 +4,23 @@
 
    // functions here will have the option to generate an appropriate url 
    // for the feed the user will want
+   
+    function FeedFormat() {
+        // maybe some setup stuff here
+    } 
+
+    FeedFormat.prototype.init = function init(argument) {
+        // settings to initialise a FeedFormat instance
+    }
+
+    // other functions to create a feed url from 
+    // 1. a user name and repo name
+    // 2. the direct url to the repository
+    // 3. the git url you can copy while cloning a repo
+    // anything else... Not hard to implement, but maybe too many options for the user...
+   
+
+    // Allow this module to be exported and used
+    module.exports = FeedFormat;
 
 });
--- repo-watch/getstuff.js
+++ repo-watch/getstuff.js
@@ -7,24 +7,30 @@
     // 
     // full repo?
     // https://github.com/devedge/GitNow/commits.atom
-    
-    var username = 'devedge';
-    var reponame = 'GitNow';
 
-    var url = 'https://github.com/' + username + '/' + reponame + '/commits.atom';
+var request = require('request');
 
-    var task_is_running = false;
-    var time_interval_in_miliseconds = 1000;
+var username = 'devedge';
+var reponame = 'GitNow';
 
-    setInterval(function(){
-        if(!task_is_running){
-            task_is_running = true;
-            console.log('yolo');
-            // do_something(42, function(result){
-            task_is_running = false;
-            // });
-            // break;
-        }
-    }, time_interval_in_miliseconds);
+var url = 'https://github.com/' + username + '/' + reponame + '/commits.atom';
+
+var keepgoing = true;
+// var task_is_running = false;
+var time_interval_in_miliseconds = 6 * 1000;
+
+setInterval(function(){
+    if(keepgoing){
+        request(url, function(err, resp, body) {
+            if(!err && (resp.statusCode === 200)) {
+                console.log(body);
+                keepgoing = false;
+            } else {
+                console.log(err);
+                keepgoing = false;
+            }
+        });
+    }
+}, time_interval_in_miliseconds);
 
 // });
