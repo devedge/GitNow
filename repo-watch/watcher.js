9256a65d7b37695f36266ef501643097fb17819c
devedge <devedge@outlook.com>
2016-06-13 23:44:25 -0400

    watcher.js flushed out more

--- repo-watch/watcher.js
+++ repo-watch/watcher.js
@@ -3,14 +3,18 @@
     'use strict';
 
     var request = require('request');
+
+    // global (withing this module) event emitter
     const EventEmitter = require('events');
-    const ee = new EventEmitter();
+    const e = new EventEmitter();
 
     var watcherInterval;
+    var feedURL;
+    var refreshtime;
 
     /*
 
-    // useage:
+    // usage:
 
     var Watcher = require('watcher');
     var gitnowrepo = new Watcher(feedurl, refreshtime);
@@ -23,20 +27,21 @@
 
     */
 
-    function Watcher(feedURL, time) {
+    function Watcher(fu, r) {
+        // initialize a new repo here
+        // need to ensure values here are valid?
+        feedURL = fu;
+        refreshtime = r;
 
-
-
-
-
-        // cb(uniqueid);
+        // do we still want to generate a unique id?
     }
 
-    // start the interval watcher
+    // Start the interval watcher
     Watcher.prototype.start = function start() {
         watcherInterval = setInterval(function() {
+            pollFeed(feedURL);
 
-        }, );
+        }, refreshtime);
     }
 
     // Kill the interval watcher
@@ -50,14 +55,24 @@
 
         request(feedURL, function(err, resp, body) {
             if(!err && (resp.statusCode === 200)) {
-                cb(null, body);
+                // cb(null, body);
+                e.emit('response', body);
             } else {
-                cb(err, null);
+                // cb(err, null);
+                e.emit('error', err);
             }
         });
     }
 
 
     // set up event emitters to handle different actions
+    e.on('err', function (err) {
+        console.log(err);
+    });
+
+    e.on('response', function (body) {
+        // check for differences, probably with a hash since that will be quicker
+        // if there are any, then handle them and notify the application
+    });
 
 })();
