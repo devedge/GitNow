80fe49f8aaed6ff914d2babfc7d9bac7ca9f54b7
devedge <devedge@outlook.com>
2016-06-16 21:20:58 -0400

    manager reorg

--- repo-watch/pollmanager.js
+++ repo-watch/pollmanager.js
@@ -18,67 +18,15 @@
     // Poll time is currently 20 minutes, but may be adjusted
     var polltime = 1200;
     
-
-    // an example feed? maybe?
-    // var feed = {
-    //     name: 'GitNow',
-    //     user: 'devedge',
-    //     feedurl: 'https://github.com/devedge/GitNow/commits/master.atom',
-    //     refreshtime: '',
-    //     live: true
-    // }
-
-    // HOW TO HANDLE USERS WITH SPACES/SPECIAL CHARACTER IN THEIR NAME
-
-
     // populate with live watchers from the config file,
     // and add new ones from the addFeed() function
     var livewatchers = {}; // an object containing a list of live watchers
     var livefeedIDs = [];  // an array containing the IDs of all the live feeds
 
-    // livefeeds = {
-    //     devedgeGitNow: Watcher {},
-    //     etc...
-    // }
-    // livefeeds.devedgeGitNow.start();
-    // livefeeds.devedgeGitNow.kill();
-
-    var feedname;
     // the unique ID is the username + reponame (github doesn't allow duplicate repos)
     // NEED TO REMOVE ANY SPECIAL CHARACTERS ('.', '_', '-', etc)
-
-    // To add a feed:
-    // feedname = '' + feed.user + feed.name;
-    // livefeeds[feedname] = new Watcher(url, time);
-
-
-    // lifeeds array usage:
-    /*
-    
-    var i = 0;
-    // keep track of which array index was started at what point in the array
-    // also store the location of the thing in some big object
-
-    // initialize
-    lifefeeds[i] = new Watcher(url, time);
-
-    // start
-    lifefeeds[i].start();
-
-    // stop
-    lifefeeds[i].kill();
-
-    */
-
-    // var feeds = {
-    //     numfeeds: 1,
-    //     active: [],
-    //     '909adb9aec6641fd30e638ee43400b7f': {
-    //         name: 'GitNow',
-    //         user: 'devedge',
-    //         feedurl: 'https://github.com/devedge/GitNow/commits/master.atom'
-    //     }
-    // }
+    // HOW TO HANDLE USERS WITH SPACES/SPECIAL CHARACTER IN THEIR NAME
+    var feedname;
 
     function PollManager(configFile) {
         // if config file is specified, try to load it
@@ -86,17 +34,6 @@
 
         // if no config file specified and there is no feed 
         // file, generate a new one
-
-
-        /*
-        var manager = new PollManager();
-        manager.addFeed(url, 60000);
-
-        var Watcher = require('watcher');
-        var x = new Watcher('https://github.com/devedge/GitNow/commits/master.atom', 2); // every 2min
-
-
-        */
 
     }
 
@@ -114,7 +51,6 @@
 
     // Add a new feed to the config object and start watching it
     PollManager.prototype.addFeed = function addFeed(feedURL) {
-
 
 
         // adds a feed, saves the feed locally (do later) and begins polling it
@@ -157,6 +93,64 @@
         // TODO
         livewatchers[feedID].start();
     }
+
+    // an example feed? maybe?
+    // var feed = {
+    //     name: 'GitNow',
+    //     user: 'devedge',
+    //     feedurl: 'https://github.com/devedge/GitNow/commits/master.atom',
+    //     refreshtime: '',
+    //     live: true
+    // }
+    // livefeeds = {
+    //     devedgeGitNow: Watcher {},
+    //     etc...
+    // }
+    // livefeeds.devedgeGitNow.start();
+    // livefeeds.devedgeGitNow.kill();
+    // To add a feed:
+    // feedname = '' + feed.user + feed.name;
+    // livefeeds[feedname] = new Watcher(url, time);
+
+
+    // lifeeds array usage:
+    /*
+    
+    var i = 0;
+    // keep track of which array index was started at what point in the array
+    // also store the location of the thing in some big object
+
+    // initialize
+    lifefeeds[i] = new Watcher(url, time);
+
+    // start
+    lifefeeds[i].start();
+
+    // stop
+    lifefeeds[i].kill();
+
+    */
+
+    // var feeds = {
+    //     numfeeds: 1,
+    //     active: [],
+    //     '909adb9aec6641fd30e638ee43400b7f': {
+    //         name: 'GitNow',
+    //         user: 'devedge',
+    //         feedurl: 'https://github.com/devedge/GitNow/commits/master.atom'
+    //     }
+    // }
+    
+    /*
+    var manager = new PollManager();
+    manager.addFeed(url, 60000);
+
+    var Watcher = require('watcher');
+    var x = new Watcher('https://github.com/devedge/GitNow/commits/master.atom', 2); // every 2min
+
+
+    */
+
 
     // this module actually polls the rss/atom feed
 
