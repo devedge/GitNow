e42264a5bcf2b17824eddf95f4160ceb657cb732
devedge <devedge@outlook.com>
2016-07-12 21:33:54 -0400

    removed a bunch of comments

--- repo-watch/pollmanager.js
+++ repo-watch/pollmanager.js
@@ -98,10 +98,6 @@
             livewatchers[element] = new Watcher(config[element].feedurl, request, notifier);
             // start it
             livewatchers[element].start();
-
-            // force an immediate check
-            livewatchers[element].force();
-
         });
     }
 
@@ -172,96 +168,6 @@
     }
 
 
-    // few other ideal methods:
-    // list all live feeds
-    // list all feeds
-
-
-    // an example feed? maybe?
-    // var feed = {
-    //     name: 'GitNow',
-    //     user: 'devedge',
-    //     feedurl: 'https://github.com/devedge/GitNow/commits/master.atom',
-    //     refreshtime: '',
-    //     live: true
-    // }
-    // livefeeds = {
-    //     devedgeGitNow: Watcher {},
-    //     etc...
-    // }
-    // livefeeds.devedgeGitNow.start();
-    // livefeeds.devedgeGitNow.kill();
-    // To add a feed:
-    // feedname = '' + feed.user + feed.name;
-    // livefeeds[feedname] = new Watcher(url, time);
-
-
-    // lifeeds array usage:
-    /*
-    
-    var i = 0;
-    // keep track of which array index was started at what point in the array
-    // also store the location of the thing in some big object
-
-    // initialize
-    lifefeeds[i] = new Watcher(url, time);
-
-    // start
-    lifefeeds[i].start();
-
-    // stop
-    lifefeeds[i].kill();
-
-    */
-
-    // var feeds = {
-    //     numfeeds: 1,
-    //     active: [],
-    //     '909adb9aec6641fd30e638ee43400b7f': {
-    //         name: 'GitNow',
-    //         user: 'devedge',
-    //         feedurl: 'https://github.com/devedge/GitNow/commits/master.atom'
-    //     }
-    // }
-    
-    /*
-    var manager = new PollManager();
-    manager.addFeed(url, 60000);
-
-    var Watcher = require('watcher');
-    var x = new Watcher('https://github.com/devedge/GitNow/commits/master.atom', 2); // every 2min
-
-
-    */
-
-
-    // this module actually polls the rss/atom feed
-
-    // set up a function that takes an array of times & feed urls
-    // for each of the respective time periods, request the atom url
-
-    // an initialization function
-    // a function for adding a repo to watch
-    // a function for removing a repo to watch
-    // an internal function to save the info about these repos to disk in a JSON file
-    // 
-
-    // maybe have two main functions:
-    // a poll manager, that manages many individual pollers
-    // 
-    // a poller, that polls a specific repository at a time. it takes
-    // care of tracking the latest commit, latest push, and loading user
-    // options from a file
-    
-    // maybe do some kind of callback or event emitter so that the notification
-    // library can be called
-
-
-
-    // keep an array of feeds being watched
-    // if application close is called, clear the entire array
-    // if a feed is paused, remove it from the array
-    // if a feed is resumed, add it back to the array
 
     module.exports = PollManager;
 
